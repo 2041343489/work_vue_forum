810286b7b851814374a0871e04d3b7b8b587d7d9
DK <dkx0828@163com>
2023-10-04 17:04:26 +0800

    2023.10.4

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
-  <div>
-    1234
+  <div class="body">
+    <indexTop />
+    <div class="content">内容</div>
+    <indexBottom />
   </div>
 </template>
 
 <script setup lang="ts">
-
+import indexTop from "@/components/index_top/index.vue";
+import indexBottom from "@/components/index_bottom/index.vue";
 </script>
 
-<style scoped>
-
+<style scoped lang="less">
+.body{
+  .content{
+    width: 80vw;
+    min-height: 500px;
+    background: #000;
+  }
+}
 </style>
